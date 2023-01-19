1c5995774af87b88c8b6dabcf2ea978e5848f36e
Neel Chheda <neel.chheda@ritetechnologies.co.in>
2023-01-19 17:16:15 +0530

    mid-day changes on 19th January

---
+++ EmployeeManagement/Employee Management/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management.Controllers {
+    public class AccountController : Controller {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+    }
+}
