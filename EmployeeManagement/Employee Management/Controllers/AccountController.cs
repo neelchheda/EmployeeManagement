fa039a40d0818bd57e8a981921054eab2087997e
Neel Chheda <neel.chheda@ritetechnologies.co.in>
2023-01-20 18:00:31 +0530

    20th january using Identity functions and authenication.

--- EmployeeManagement/Employee Management/Controllers/AccountController.cs
+++ EmployeeManagement/Employee Management/Controllers/AccountController.cs
@@ -1,11 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Employee_Management.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Employee_Management.Controllers {
-    public class AccountController : Controller {
+    public class AccountController : Controller 
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+           await signInManager.SignOutAsync();
+            return RedirectToAction("index","home");
+        }
+
+
         [HttpGet]
         public IActionResult Register()
         {
             return View("Register");
+        }
+        [HttpPost]
+        public async Task <IActionResult> Register(RegisterUserViewModel model)
+        {
+            if(ModelState.IsValid) 
+            {
+                var user  = new IdentityUser { UserName = model.Email,Email = model.Email };
+              var result= await userManager.CreateAsync(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user,isPersistent:false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                } else { 
+               
+                    ModelState.AddModelError(string.Empty, "Invalid Login Credentials");
+                }
+            }
+
+            return View(model);
         }
     }
 }
